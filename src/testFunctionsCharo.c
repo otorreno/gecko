2730a2b62f22b1b0afdb0f6f9a96b60a497be9a4
Charo Garcia Morales <rosariogar@uma.es>
2021-11-09 13:57:25 +0100

    Cambios y pruebas de lo que llevo hecho

--- src/testFunctionsCharo.c
+++ src/testFunctionsCharo.c
@@ -4,22 +4,41 @@
 #include <sys/types.h>
 #include <math.h>
 
+
 #include "fragmentv3.h"
 #include "fragmentv2.h"
 #include "lista.h"
 #include "comparisonFunctions.h"
 #define  MAX_LEVELS  900000
 
+void terror(char*);
+
 int main(int ac,char** av){
 	
 	// Read fragments
 	struct FragFile *f;
 	int nf; // Number of fragments
+	int n = 0;
 	uint64_t xtotal,ytotal;
 	nf = 0;
     FILE *fichero;
+
 	f = readFragmentsv2(av[1],&nf,&xtotal,&ytotal);
-    fichero = fopen("ficheroCharo.txt", "rb");
-	writeFragmentRaw(&f[0], fichero);
+    fprintf(stdout,"%" PRIu64"\n", xtotal);
+	fprintf(stdout,"%" PRIu64"\n", ytotal);
+	fprintf(stdout,"%" PRIu64"\n", nf);
+	fichero = fopen(av[2], "wb");
+	if(fichero == NULL){
+		terror("Abriendo fichero de salida");
+	}
+	fwrite(&xtotal,sizeof(uint64_t), 1, fichero);
+	fwrite(&ytotal,sizeof(uint64_t), 1, fichero);
+	for(n;n < nf; n++){
+		writeFragmentRaw(&f[n], fichero);
+		
+	}
+
+	fclose(fichero);
 	return 0;
+	
 }
